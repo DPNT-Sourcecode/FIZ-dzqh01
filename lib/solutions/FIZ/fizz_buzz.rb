0e2ca6595aeae566d7daa12124df7024471a4381
sbx_user1051 <sbx_user1051@169.254.19.93>
2019-04-08 13:05:48 +0000

    Mon Apr 08 13:05:48 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,13 +2,23 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    final = ''
-    final += 'fizz buzz' if number % 5 == 0 && number % 3 == 0 || (number.to_s.include?('3') && number.to_s.include?('5'))
-    final += ' fizz' if number % 3 == 0 || number.to_s.include?('3')
-    final += ' buzz' if number % 5 == 0 || number.to_s.include?('5')
-    # return number
+
     final
+
+    if number % 3 == 0
+      final += 'fizz'
+    else
+      return number
+    end
+
+    # final = ''
+    # final += 'fizz buzz' if number % 5 == 0 && number % 3 == 0 || (number.to_s.include?('3') && number.to_s.include?('5'))
+    # final += ' fizz' if number % 3 == 0 || number.to_s.include?('3')
+    # final += ' buzz' if number % 5 == 0 || number.to_s.include?('5')
+    # # return number
+    final.strip!
   end
 end
+
 
 
