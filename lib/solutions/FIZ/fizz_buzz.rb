812f8eed45907e2ed94d3c10d7b9b1a0d567c17b
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:52:43 +0000

    Mon Apr 08 10:52:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,9 +3,10 @@ class FizzBuzz
 
   def fizz_buzz(number)
     return 'fizz buzz' if number % 5 == 0 && number % 3 == 0
-    return 'fizz' if number % 3 == 0
+    return 'fizz' if number % 3 == 0 || number.to_s.chars.map(&:to_i)
     return 'buzz' if number % 5 == 0
     return number
   end
 
 end
+
