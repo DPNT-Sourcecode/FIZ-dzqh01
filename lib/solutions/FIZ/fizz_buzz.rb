06b6cb6aac71e9093f972d67cdbaaea711640f0d
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 11:10:43 +0000

    Mon Apr 08 11:10:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -9,6 +9,3 @@ class FizzBuzz
   end
 
 end
-
-
-
--- test/FIZ/fizz_test.rb
+++ test/FIZ/fizz_test.rb
@@ -12,10 +12,8 @@ class ClientTest < Minitest::Test
     assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return "fizz" when given 3'
     assert(FizzBuzz.new.fizz_buzz(13) == 'fizz', 'App should return "fizz" when the number has a 3 in it')
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'App should return "buzz" when given 5'
-    assert(FizzBuzz.new.fizz_buzz(51) == 'buzz', 'App should return "buzz" when the number has a 5 in it')
+    assert(FizzBuzz.new.fizz_buzz(52) == 'buzz', 'App should return "buzz" when the number has a 5 in it')
     assert_equal 'fizz buzz', FizzBuzz.new.fizz_buzz(15), 'App should return "fizz buzz" when given 15'
   end
 
 end
-
-
