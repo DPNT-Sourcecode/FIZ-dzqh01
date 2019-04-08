3058c4891b59ae62463208904b99843fbc120d7a
sbx_user1051 <sbx_user1051@169.254.19.93>
2019-04-08 13:14:36 +0000

    Mon Apr 08 13:14:36 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,9 +6,13 @@ class FizzBuzz
     final = ''
     if (number % 3).zero? && (number % 5).zero? || number.to_s.include?('3') && number.to_s.include?('5')
       final += 'fizz buzz'
-    elsif (number % 3).zero? || number.to_s.include?('3')
+    elsif (number % 3).zero?
       final += 'fizz'
-    elsif (number % 5).zero? || number.to_s.include?('5')
+    elsif number.to_s.include?('3')
+      final += 'fizz'
+    elsif (number % 5).zero?
+      final += 'buzz'
+    elsif number.to_s.include?('5')
       final += 'buzz'
     else
       return number
@@ -22,3 +26,4 @@ class FizzBuzz
     final.strip
   end
 end
+
